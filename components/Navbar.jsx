5b66789c0d1d9cca5754cf3888d797de306e05b9
Mogakamo <mogaka.amo254@gmail.com>
2022-02-01 12:20:59 +0300

    commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,14 +3,14 @@ import DarkmodeSwitch from './DarkmodeSwitch'
 
 function Navbar() {
   return (
-    <div className="sticky top-0 h-18 shadow-lg">
-      <div className='relative'>
-      <div className=''>
-        <h1>Navbar</h1>
-      </div>
-      <div className="">
-        <DarkmodeSwitch />
-      </div>
+    <div className="h-18 sticky top-0 shadow-lg">
+      <div className="grid grid-cols-2 py-5 ">
+        <div className="px-10">
+          <h1 className="font-bold text-xs md:text-lg">Where in the world?</h1>
+        </div>
+        <div className="hidden md:inline-grid mr-0">
+          <DarkmodeSwitch />
+        </div>
       </div>
     </div>
   )
